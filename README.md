ddf7786b66ab6fa3431279b293c1eba60f145ee4
Lambay mathéo <matheolambay@gmail.com>
2024-04-27 12:18:31 +0200

    mini projet final update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mini-projet
-capteur de flamme 
+## capteur de flamme 
+
+* capteur = Pin(16, Pin.IN)
+* buzzer = Pin(17,Pin.OUT)
+* frequency = 15000
+* pin15 = Pin(15, Pin.OUT)
+* pin14 = Pin(14, Pin.OUT)
+* enable = PWM(Pin(13), frequency)
+* dc_motor = DCMotor(pin15, pin14, enable)
+* btn = Pin(12,Pin.IN,Pin.PULL_DOWN)
+* red = Pin(19,Pin.OUT)
+* blue = Pin(18,Pin.OUT)
+* buzzer.low()
--- main.py
+++ main.py
@@ -1,30 +1,77 @@
 from machine import Pin, PWM
 from dcmotor import DCMotor
-from time import sleep
+import uasyncio 
+#code capteur de flamme by
+#Lambay Mathéo
+#Ouaffi Zakaria
 
-capteur = Pin(16, Pin.IN)
-buzzer = Pin(17,Pin.OUT)
-frequency = 15000
-pin15 = Pin(15, Pin.OUT)
-pin14 = Pin(14, Pin.OUT)
-enable = PWM(Pin(13), frequency)
-dc_motor = DCMotor(pin15, pin14, enable)
-btn = Pin(12,Pin.IN,Pin.PULL_DOWN)
-red = Pin(19,Pin.OUT)
-blue = Pin(18,Pin.OUT)
+class FireDetector:
+    def __init__(self,Pin_capteur,Pin_buzzer,Pin_btn,Pin_red,Pin_green,Pin_forward,Pin_backward,Pin_enable): 
+        self.capteur = Pin(Pin_capteur, Pin.IN)
+        self.buzzer = Pin(Pin_buzzer,Pin.OUT)
+        self.Pin_forward = Pin(Pin_forward,Pin.OUT)
+        self.Pin_backward = Pin(Pin_backward,Pin.OUT)
+        self.enable = PWM(Pin(Pin_enable),15000)
+        self.dc_motor = DCMotor(self.Pin_forward,self.Pin_backward,self.enable)
+        self.button = Pin(Pin_btn,Pin.IN,Pin.PULL_DOWN)
+        self.red = Pin(Pin_red,Pin.OUT)
+        self.green = Pin(Pin_green,Pin.OUT)
+        #mise à zéro des variables et des actionneurs 
+        self.buzzer.low()
+        self.green.high()
+        self.red.low()
+        self.dc_motor.stop()
+        self.isFire = 0
 
-     
-while 1:
-    if not(capteur.value()):
-        dc_motor.forward(100)
-        red.value(1)
-        blue.value(0)
-        while btn.value() == 0:
-            buzzer.high()
-            sleep(1)
-            buzzer.low()
-            sleep(2)
-    blue.value(1)
-    red.value(0)
-    dc_motor.stop()
-    sleep(0.2)
+    async def detectFire(self): #passe le flag isFire sur 1 lorsque la flamme est détecté
+        while 1:
+            if not(self.capteur.value()): 
+                self.isFire = 1
+            await uasyncio.sleep(0.1)
+
+    async def ventillo(self): #allume le ventillo ou l'arrête
+        while 1:
+            if self.isFire:
+                self.dc_motor.forward(100)
+            else:
+                self.dc_motor.stop()
+            await uasyncio.sleep(0.1)
+
+    async def btnPressed(self): # reset du flag isFire
+        while 1:
+            if self.button.value():
+                self.isFire = 0  
+            await uasyncio.sleep(0.1)
+
+    async def ledOn(self): #gère la couleur de la led RGB
+        while 1:
+            if self.isFire:
+                self.green.low()
+                self.red.high()
+            else:
+                self.green.high()
+                self.red.low()
+            await uasyncio.sleep(0.1)
+
+    async def buzzerOn(self): #fait bipper le buzzer toutes les secondes
+        while 1:
+            if self.isFire:
+                self.buzzer.high()
+                await uasyncio.sleep(1)
+                self.buzzer.low()
+                await uasyncio.sleep(1)
+            else:
+                self.buzzer.low()
+            await uasyncio.sleep(0.1)
+
+async def main(): #initialisation du capteur et appelle des fonction asynchrone. 
+    capteur = FireDetector(16,17,12,19,18,15,14,13)
+    while 1:
+        uasyncio.create_task(capteur.detectFire())
+        uasyncio.create_task(capteur.ventillo())
+        uasyncio.create_task(capteur.btnPressed())
+        uasyncio.create_task(capteur.ledOn())
+        uasyncio.create_task(capteur.buzzerOn())
+        await uasyncio.sleep(10)
+
+uasyncio.run(main())
